df3419a00dfebe9f02d76e9011caebeb9c0c22e6
christiandacles10 <yanyan12ros@gmail.com>
2024-12-11 08:05:40 +0800

    first commit

---
+++ routes/booksRoutes.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const { getAllBooks,getBooksByGenre} = require("../controllers/booksController.js");
+router.get("/",getAllBooks);
+router.get("/:genre",getBooksByGenre);
+module.exports = router;
